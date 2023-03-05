54bd077b42d08e85d51afa777c0561746ea0ec09
Pedro Neto <pedro.rmn19@gmail.com>
2023-03-05 12:43:56 -0300

    Alteração do vue

---
+++ project2/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+
+  <main>
+    <h1>VUE JS</h1>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
