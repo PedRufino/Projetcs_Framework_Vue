eadb27685268b092d4f54831f354f0b165c6a23b
Pedro Neto <pedro.rmn19@gmail.com>
2023-03-05 15:53:14 -0300

    First Projects

---
+++ project3_componentes/src/components/Texto.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Título</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quia debitis consequatur error temporibus! Incidunt ab voluptatibus libero sapiente, quaerat laboriosam quam, vero, beatae tempore iure sit neque magnam voluptate?</p>
+</template>
+
+
+<script>
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        name: 'Texto'
+    })
+</script>
+
+
+<style>
+
+</style>
