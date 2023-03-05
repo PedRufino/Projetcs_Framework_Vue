e1115650bd91178d06a10d13b9b7ec4baf787818
Pedro Neto <pedro.rmn19@gmail.com>
2023-03-05 16:45:33 -0300

    Estrutura repetição

--- project4_condicional/src/App.vue
+++ project4_condicional/src/App.vue
@@ -1,47 +1,60 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+  import logo from './assets/logo.svg'
+
+  export default {
+    name:'App',
+    data(){
+      return{
+        titulo:'<b>Consumindo API em PHP com vue</b>',
+        mostra:true,
+        nomes:[
+          {'id':1, 'nome':'João'},
+          {'id':2, 'nome':'Maria'},
+          {'id':3, 'nome':'José'},
+          {'id':4, 'nome':'Ana'}
+        ],
+        logo: logo
+      }
+    },
+    methods:{
+      alert(){
+        alert('Você pressionou o botão!!')
+      },
+      alert2(){
+        alert('Olaaaa!!!')
+      }
+    }
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div id="app">
+    <img :src="logo" alt="imagem">    
+    <h1>Vue JS</h1>
+    <h2 v-html="titulo"></h2>
+    <p v-if="true">Mostra o Texto</p>
+    <p v-if="mostra">Mostra o texto com a variável</p>
+    <div v-if="Math.random() > 0.5">
+      Agora você me vê
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div v-else>
+      Agora não vê
+    </div>
+    <h2 v-for="(nom,index) in nomes" :key="index" id="cor">
+      {{ nom.id }} | {{ nom.nome }}
+    </h2>
+    <button v-on:click="alert">Clique Aqui</button><br>
+    <button @:click="alert2">Clique Aqui</button>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style>
+  @import './assets/base.css';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  #app{
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+    font-weight: normal;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
