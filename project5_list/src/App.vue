68da65ad241f8d762c0cb7099c8022425c782877
Pedro Neto <pedro.rmn19@gmail.com>
2023-03-05 17:06:41 -0300

    projeto lista

--- project5_list/src/App.vue
+++ project5_list/src/App.vue
@@ -15,9 +15,6 @@ export default {
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  </header>
   <table border="1">
     <tbody>
       <th>ID</th>
