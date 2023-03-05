3295e71714979b2d70d534e97520ef4509f083e6
Pedro Neto <pedro.rmn19@gmail.com>
2023-03-05 13:04:46 -0300

    Project3

---
+++ project3_componentes/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
