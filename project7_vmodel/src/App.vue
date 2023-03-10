435f3a5d361e1c57523865033aeca97d28ebfd2e
Pedro Neto <pedro.rmn19@gmail.com>
2023-03-09 21:53:26 -0300

    adicionado string em variavel pelo input; criando componentes

--- project7_vmodel/src/App.vue
+++ project7_vmodel/src/App.vue
@@ -1,47 +1,27 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import Cabecalho from './components/Cabecalho.vue'
+  export default{
+    name:'App',
+    components:{
+      Cabecalho
+    },
+    data(){
+      return{
+        msg: ''
+      }
+    }
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <Cabecalho/>
+  <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <input id="txt" v-model="msg" placeholder="Comece a digitar"><br/>
+  <p>A mensagem é: {{ msg }}</p>
+  <span v-pre="{{ msg }}"></span>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ project7_vmodel/src/components/Cabecalho.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container-fluid">
+        <div class="bg-info">
+            {{ titulo }}
+        </div>
+
+        <nav class="navbar navbar-expand-lg navbar-light hg-light">
+
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'cabecalho',
+        data(){
+            return{
+                titulo: 'Exemplo de Cabeçalho'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
