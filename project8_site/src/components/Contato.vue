bb4fcdd5ffcf4640c1dc0caadbe11b82f192d722
Pedro Neto <pedro.rmn19@gmail.com>
2023-03-27 20:33:53 -0300

    Contato

--- project8_site/src/components/Contato.vue
+++ project8_site/src/components/Contato.vue
@@ -1,36 +1,31 @@
 <template>
     <div class="row" style="margin-left: 100px; margin-right: 100px; margin-top: 10rem;">
-        <img class="col-6" style="padding: 0 !important;" src="https://mangakun.com/wp-content/uploads/2023/02/Como-ler-manga.webp" alt="">
-        <form class="form-control col needs-validation" novalidate>
+        <img class="col-6" style="padding: 0 !important;" src="https://s5.static.brasilescola.uol.com.br/be/2022/04/pessoa-lendo-um-manga.jpg" alt="">
+        <form class="form-control bg-meu col needs-validation" novalidate>
             <div class="row g-3">
                 <div class="col-md-12">
                     <label for="validationCustom01" class="form-label">Nome:</label>
-                    <input type="text" class="form-control" id="validationCustom01" value="Mark" required>
+                    <input type="text" class="form-control" id="validationCustom01" placeholder="Digite seu nome" minlength="3" required>
                     <div class="valid-feedback">
                         Parece bom!
                     </div>
                 </div>
                 <div class="col-md-12">
                     <label for="validationCustom02" class="form-label">Sobrenome:</label>
-                    <input type="text" class="form-control" id="validationCustom02" value="Otto" required>
+                    <input type="text" class="form-control" id="validationCustom02" placeholder="Digite seu sobrenome" minlength="3" required>
                     <div class="valid-feedback">
                         Parece bom!
                     </div>
                 </div>
                 <div class="col-md-12">
-                    <label for="validationCustomUsername" class="form-label">Nome de usuário:</label>
-                    <div class="input-group has-validation">
-                        <span class="input-group-text" id="inputGroupPrepend">@</span>
-                        <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend"
-                            required>
-                        <div class="invalid-feedback">
-                            Escolha um nome de usuário.
-                        </div>
+                    <label for="validationDefaultEmail" class="form-label">E-mail:</label>
+                    <div class="input-group">
+                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Digite seu email" required>
                     </div>
                 </div>
                 <div class="col-md-12">
                     <label for="validationCustom03" class="form-label">Cidade:</label>
-                    <input type="text" class="form-control" id="validationCustom03" required>
+                    <input type="text" class="form-control" id="validationCustom03" placeholder="Digite sua ciade" required>
                     <div class="invalid-feedback">
                         Forneça uma cidade válida.
                     </div>
@@ -38,8 +33,11 @@
                 <div class="col-md-12">
                     <label for="validationCustom04" class="form-label">Estado:</label>
                     <select class="form-select" id="validationCustom04" required>
-                        <option selected disabled value="">Choose...</option>
-                        <option>...</option>
+                        <option selected disabled value="">Escolha</option>
+                        <option>SP</option>
+                        <option>RJ</option>
+                        <option>MG</option>
+                        <option>AL</option>
                     </select>
                     <div class="invalid-feedback">
                         Selecione um estado válido.
@@ -47,29 +45,51 @@
                 </div>
                 <div class="col-md-12">
                     <label for="validationCustom05" class="form-label">CEP</label>
-                    <input type="text" class="form-control" id="validationCustom05" required>
+                    <input type="text" class="form-control " id="validationCustom05" placeholder="Digite seu CEP" minlength="9" maxlength="9" required>
                     <div class="invalid-feedback">
                         Forneça um CEP válido.
                     </div>
                 </div>
                 <div class="col-12">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-                        <label class="form-check-label" for="invalidCheck">
-                            Concordo com os termos e condições
-                        </label>
-                        <div class="invalid-feedback">
-                            Você deve concordar antes de enviar.
+                    <div class="m-3 row">
+                        <div class=" col form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+                            <label class="form-check-label" for="invalidCheck">
+                                Concordo com os termos e condições
+                            </label>
+                            <div class="invalid-feedback">
+                                Você deve concordar antes de enviar.
+                            </div>
+                        </div>
+                        <div class="col form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+                            <label class="form-check-label" for="invalidCheck">
+                                Desejo receber noticias por e-mail
+                            </label>
                         </div>
                     </div>
                 </div>
                 <div class="col-12">
-                    <button class="btn btn-primary" type="submit">Enviar</button>
+                    <center>
+                        <button class="btn btn-size btn-primary" type="submit">Enviar</button>
+                    </center>
                 </div>
             </div>
         </form>
     </div>
 </template>
+
+<style>
+.bg-meu{
+    background-color: rgb(158, 189, 255)!important;
+    border: 0 !important;
+    border-radius: 0 !important;
+    padding: 2vw !important;
+}
+.btn-size{
+    width: 50%;
+}
+</style>
 
 <script>
     import { defineComponent } from 'vue';
