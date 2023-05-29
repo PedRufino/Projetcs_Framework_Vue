9d412e842f1c786740db2eac333643f712f7df10
Pedro Neto <pedro.rmn19@gmail.com>
2023-05-29 20:39:16 -0300

    Criação do projeto 11 - API

---
+++ project11_API/src/components/DadosApi.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <form class="form-control" v-if="loading==false">
+            <label>CEP:</label>
+            <p><input class="form-control" type="text" v-model="dados.cep" v-on:keyup.enter="buscaDados(dados)"/></p>
+            <label>Logradouro:</label>
+            <p><input class="form-control" type="text" v-model="dados.logradouro"/></p>
+            <label>Bairro:</label>
+            <p><input class="form-control" type="text" v-model="dados.bairro"/></p>
+            <label>Localidade:</label>
+            <p><input class="form-control" type="text" v-model="dados.localidade"/></p>
+            <label>UF:</label>
+            <p><input class="form-control" type="text" v-model="dados.uf"/></p>
+        </form>
+        <img class="img-loading" src="../assets/loading.gif" width="50" v-if="loading" alt=""/>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { http } from '@/servicos/config';
+export default defineComponent({
+    name: 'DadosApi',
+    data(){
+        return{
+            loading: false,
+            dados:{cep:'', logradouro:'', bairro:'', localidade:'', uf:''}
+        }
+    },
+    methods:{
+        async buscaDados(dados:any){
+            try{
+                this.loading = true;
+                const response = await http.get('https://viacep.com.br/ws/'+dados.cep+'/json/');
+                this.dados = response.data;
+                this.loading = false;
+            } catch (error) {
+                this.loading = false;
+                console.log(error)
+            }
+        }
+    }
+})
+</script>
+
+<style>
+
+</style>
